9a91128433eaed1419eb3974e3299643a99a2d71
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-11 21:01:28 -0500

    Adding proposal and updating code

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # LED Prop Module
 #### Nabih Estefan & Olivia Jo Bradley
-![Main Photo](imgs/mainPhoto.jpg)
+![Main Photo](images/mainPhoto.jpg)
 
 For this project, we wanted to create a LED Prop Module by creating a battery powered PCB with a microcontroller, LEDs, an accelerometer, and buttons ,which would be enclosed in a clear capsule. 
 Both of us are members of the Olin Fire Arts Club, and enjoy springing and performing with fire. Spinning fire is dangerous, and fire props are delicate, so we try to avoid practicing and trying new tricks with them. The solution to this is an LED version of the prop that is more durable and less dangerous than fire props, but is still exciting and aesthetic, so it can be used to perform with if desired.
 This project requires a board with a battery management system, bright RGBW LEDs, an accelerometer (to react to the movements of spinning), a button to change modes, and a microcontroller that can run the code to make all of these work. As a stretch goal, we also wanted to add a bluetooth module to the board so we could connect and sync the colors and patterns of multiple modules. 
 
 
-This repository holds the [Software](Software/), [Electrical](Electrical/)and [CAD](CAD/) files required to fabricate and assemble the board and casing designed by us and teh code to run the Prop Module. It also has our [Report](Report.pdf) detailing how we designed and built the Prop module.
+This repository holds the [Software](Software/), [Electrical](Electrical/)and [CAD](CAD/) files required to fabricate and assemble the board and casing designed by us and teh code to run the Prop Module. There is also a [images](images/) folder that contains images of the prototype and demo videos of the prop module in use. Finally, it also has our [Proposal](Proposal.pdf) and [Report](Report.pdf) detailing how we designed and built the Prop module.
--- Software/main.py
+++ Software/main.py
@@ -1,21 +1,34 @@
 from machine import Pin, Timer, SPI
 import neopixel
+import ADXL362 as accel
+
+timer = Timer()
 
 # COLORS
-RED_COLOR = (255, 0, 0)
-BLUE_COLOR = (0, 50, 255)
-GREEN_COLOR = (0,255,0)
-YELLOW_COLOR = (255, 239, 0)
+RED = (255, 0, 0)
+BLUE = (0, 50, 255)
+GREEN = (0,255,0)
+YELLOW = (255, 239, 0)
+WHITE = (255, 255, 255)
 
-COLORS = [RED_COLOR, GREEN_COLOR, BLUE_COLOR, YELLOW_COLOR]
+COLORS = [[WHITE],
+          [RED, WHITE],
+          [BLUE,WHITE],
+          [GREEN,WHITE],
+          [YELLOW,WHITE],
+          [RED,BLUE],
+          [BLUE,GREEN],
+          [BLUE,YELLOW],
+          [RED, BLUE, GREEN]]
 
-COLOR_IND = 0
+COLOR_IND = -1
+COLOR_SUB_IND=0
 
 # TIMES
 STATIONARY_TIME = 1.6
 MOVING_TIME = 0.4
 
-MOVE_THRESHOLD = 250
+MOVE_THRESHOLD = 1
 
 NUM_PIXELS = 8
 
@@ -26,40 +39,104 @@ LED_PIN = Pin(25, Pin.OUT)
 
 strip = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_PIXELS)
 
-strip.fill(COLORS[COLOR_IND])
+strip.fill(COLORS[COLOR_IND][COLOR_SUB_IND])
 strip.write()
 
 
-mode_button = DigitalInOut(BUTTON_PIN)
-mode_button.direction = Direction.INPUT
-mode_button.pull = Pull.UP                        
 
-time.sleep(0.1)
+# mode_button = DigitalInOut(BUTTON_PIN)
+# mode_button.direction = Direction.INPUT
+# mode_button.pull = Pull.UP                        
 
 # Accelerometer
-bus = SPI(0, sck = Pin(18, Pin.OUT), miso = Pin(16, Pin.IN), mosi = Pin(19, Pin.OUT))
-# X axis reg 0x08
-# X axis reg 0x09
-# X axis reg 0x0A
+accel.begin_measure()
 
+# Accelerometer
+spi = SPI(0,
+          baudrate=1000000,
+          polarity=1,
+          phase=1,
+          bits=8,
+          firstbit=machine.SPI.MSB,
+          sck=machine.Pin(18),
+          mosi=machine.Pin(19),
+          miso=machine.Pin(16))
+
+cs = Pin(17, mode=Pin.OUT, value=1)
+cs.high()
+
+DEVID = 0xAD
+REG_DEVID = 0x00
+REG_X     = 0x08
+REG_Y     = 0x09
+REG_Z     = 0x0A
+
+def reg_read(spi, cs, reg):    
+    # Construct message (set ~W bit high)
+    msg = bytearray()
+    msg.append(0x0B)
+    msg.append(reg)
+    print(msg)
+    
+    data = bytearray(1)
+    
+    # Send out SPI message and read
+    cs.low()
+    spi.write(msg)
+
+    spi.readinto(data, 0x00)
+    cs.high()
+    
+    print(data)
+    return data
+
+
+def mainChange(timer):
+    global COLORS
+    global COLOR_IND
+    global COLOR_SUB_IND
+    COLOR_SUB_IND = (COLOR_SUB_IND+1)%len(COLORS[COLOR_IND])
+    strip.fill(COLORS[COLOR_IND][COLOR_SUB_IND])
+    strip.write()
+    
+    
 moving = False
+timer.init(freq=1, mode=Timer.PERIODIC, callback=mainChange)
+
+reg_read(spi, cs, REG_DEVID)
+data = reg_read(spi, cs, REG_DEVID)
+if data != bytearray((DEVID, )):
+    print("ERROR: Could not communicate with ADXL362")
+    sys.exit()
 
 # Main program loop, repeats indefinitely
-while True:
-
-    if not change.value:                  			# button pressed
-        COLOR_IND = (COLOR_IND+1)%len(COLORS) 		# move on to next color
-        strip.fill(COLORS[COLOR_IND])            	# Fill and show color on strip              
-        strip.show()
-
-    ## This is the accelerometer math (TODO HERE)
-#     x, y, z = accel.acceleration # Read accelerometer
-#     accel_total = x * x + z * z + y * y
-#     
-#     if accel_total > MOVE_THRESHOLD:   # Poi is moving
-#         COLOR_IND = (COLOR_IND+2)%len(COLORS)
-#         time.sleep(0.1)# move on to next color
+# while True:
+# 
+#     if not change.value:                  			# button pressed
+#         COLOR_IND = (COLOR_IND+1)%len(COLORS) 		# move on to next color
 #         strip.fill(COLORS[COLOR_IND])            	# Fill and show color on strip              
 #         strip.show()
-#         accel_total = 1
+# 
+#     # Read 3 axes of accelerometer
+#     x = reg_read(spi, cs, REG_X)
+#     y = reg_read(spi, cs, REG_Y)
+#     z = reg_read(spi, cs, REG_Z)
+
+#     x = accel.read_x()
+#     y = accel.read_y()
+#     z = accel.read_z()
+
+#     print(x, y, z)
+#     print(type(x))
+# 
+#     accel_total = x * x + z * z + y * y
+#     
+#     if accel_total > MOVE_THRESHOLD and not moving:   # Poi is moving
+#         strip.fill(COLORS[GREEN_COLOR])
+#         strip.write()
+#         timer.deinit()
+#         timer.init(freq=4, mode=Timer.PERIODIC, callback=mainChange)
+#     elif moving:
+#         timer.deinit()
+#         timer.init(freq=1, mode=Timer.PERIODIC, callback=mainChange)
     
